53f2834dd66f7a3273fa130bac0f25e7904fc68e
almeida_lucas <almeida.lucas92@gmail.com>
2018-06-08 17:32:33 -0300

    Adicionado eventos que detectam novo cliente conectado e cliente desconectado. NodeController.java

--- src/main/java/com/sistemas/distribuidos/DistributionManager/Controller/NodeController.java
+++ src/main/java/com/sistemas/distribuidos/DistributionManager/Controller/NodeController.java
@@ -1,8 +1,13 @@
 package com.sistemas.distribuidos.DistributionManager.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 /**
  * @author Lucas Almeida
@@ -16,5 +21,20 @@ public class NodeController {
   public String sendToNode(String message) throws Exception {
     System.out.println("NodeController - " + message);
     return message;
+  }
+
+  /*Emissão de logs*/
+  private static final Logger logger = LoggerFactory.getLogger(NodeController.class);
+
+  /*Evento para detectar novo cliente conectado*/
+  @EventListener
+  public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+    logger.info("Nova conexão!");
+  }
+
+  /*Evento para detectar cliente desconectado*/
+  @EventListener
+  public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+    System.out.println("Usuário desconectado!");
   }
 }
